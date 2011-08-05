fe8b9b526b601d2ef0e8ddcf2a0d2e38c66455cd
Tal Rotbart <redbeard@gmail.com>
2011-08-05 10:42:09 +1000

    Support for automatic retry on json request to API

--- src/main/java/com/springsense/disambig/MeaningRecognitionAPI.java
+++ src/main/java/com/springsense/disambig/MeaningRecognitionAPI.java
@@ -3,7 +3,10 @@ package com.springsense.disambig;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.*;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,118 +15,172 @@ import java.util.Map;
  */
 public class MeaningRecognitionAPI {
 
-    private String url;
-    private String customerId;
-    private String apiKey;
-    private Proxy proxy;
+	private String url;
+	private String customerId;
+	private String apiKey;
+	private Proxy proxy;
 
-    /**
-     * Create a Meaning Recognition API entry point with the specified end-point URL, customer id and API key
-     * @param url The end-point URL to use. Most likeley http://api.springsense.com/disambiguate
-     * @param customerId Your customer id, get yours at http://springsense.com/api
-     * @param apiKey Your secret API key
-     */
-    public MeaningRecognitionAPI(String url, String customerId, String apiKey) {
-        this(url, customerId, apiKey, null);
-    }
+	private int numberOfRetries = 3;
+	private long waitBetweenRetries = 2000;
 
-    /**
-     * Create a Meaning Recognition API entry point with the specified end-point URL, customer id and API key, going through the specified proxy
-     * @param url The end-point URL to use. Most likeley http://api.springsense.com/disambiguate
-     * @param customerId Your customer id, get yours at http://springsense.com/api
-     * @param apiKey Your secret API key
-     * @param proxy The Proxy to use for communications
-     */
-    public MeaningRecognitionAPI(String url, String customerId, String apiKey, Proxy proxy) {
-        this.url = url;
-        this.customerId = customerId;
-        this.apiKey = apiKey;
-        this.proxy = proxy;
-    }
+	/**
+	 * Create a Meaning Recognition API entry point with the specified end-point
+	 * URL, customer id and API key
+	 * 
+	 * @param url
+	 *            The end-point URL to use. Most likeley
+	 *            http://api.springsense.com/disambiguate
+	 * @param customerId
+	 *            Your customer id, get yours at http://springsense.com/api
+	 * @param apiKey
+	 *            Your secret API key
+	 */
+	public MeaningRecognitionAPI(String url, String customerId, String apiKey) {
+		this(url, customerId, apiKey, null);
+	}
 
-    String getApiKey() {
-        return apiKey;
-    }
+	/**
+	 * Create a Meaning Recognition API entry point with the specified end-point
+	 * URL, customer id and API key, going through the specified proxy
+	 * 
+	 * @param url
+	 *            The end-point URL to use. Most likeley
+	 *            http://api.springsense.com/disambiguate
+	 * @param customerId
+	 *            Your customer id, get yours at http://springsense.com/api
+	 * @param apiKey
+	 *            Your secret API key
+	 * @param proxy
+	 *            The Proxy to use for communications
+	 */
+	public MeaningRecognitionAPI(String url, String customerId, String apiKey,
+			Proxy proxy) {
+		this.url = url;
+		this.customerId = customerId;
+		this.apiKey = apiKey;
+		this.proxy = proxy;
+	}
 
-    String getCustomerId() {
-        return customerId;
-    }
+	String getApiKey() {
+		return apiKey;
+	}
 
-    String getUrl() {
-        return url;
-    }
+	String getCustomerId() {
+		return customerId;
+	}
 
-    /**
-     * Makes a remote procedure call to the Meaning Recognition API server and attempts to disambiguate the specified text
-     * @param textToRecognize The text to recognize, limited to 512 characters.
-     * @return The disambiguated (recognized) result
-     * @throws Exception In case of a communications or security error
-     */
-    public DisambiguationResult recognize(String textToRecognize) throws Exception {
-        final String jsonResponse = callRestfulWebService(getAuthorizationParameters(), textToRecognize);
+	String getUrl() {
+		return url;
+	}
+
+	public int getNumberOfRetries() {
+		return numberOfRetries;
+	}
+
+	public void setNumberOfRetries(int numberOfRetries) {
+		this.numberOfRetries = numberOfRetries;
+	}
+
+	public long getWaitBetweenRetries() {
+		return waitBetweenRetries;
+	}
+
+	public void setWaitBetweenRetries(long waitBetweenRetries) {
+		this.waitBetweenRetries = waitBetweenRetries;
+	}
+
+	/**
+	 * Makes a remote procedure call to the Meaning Recognition API server and
+	 * attempts to disambiguate the specified text
+	 * 
+	 * @param textToRecognize
+	 *            The text to recognize, limited to 512 characters.
+	 * @return The disambiguated (recognized) result
+	 * @throws Exception
+	 *             In case of a communications or security error
+	 */
+	public DisambiguationResult recognize(String textToRecognize) throws Exception {
+        String jsonResponse = null;
+        int attempt = 0;
+        
+        while (jsonResponse == null) { 
+        	attempt++;
+        	try {
+        		jsonResponse = callRestfulWebService(getAuthorizationParameters(), textToRecognize);
+        	} catch (Exception e) {
+        		if (attempt > getNumberOfRetries()) {
+        			throw new RuntimeException(String.format("Tried %d times, but still could not disambiguate '%s'. Latest error attached.", attempt, textToRecognize), e);
+        		}
+        		Thread.sleep(waitBetweenRetries);
+        	}
+        	
+        }
+        
         return DisambiguationResult.fromJson(jsonResponse);
     }
 
-    private Map<String, String> getAuthorizationParameters()
-    {
-        Map<String, String> map = new HashMap<String, String>();
+	private Map<String, String> getAuthorizationParameters() {
+		Map<String, String> map = new HashMap<String, String>();
 
-        map.put("customerId", getCustomerId());
-        map.put("apiKey", getApiKey());
+		map.put("customerId", getCustomerId());
+		map.put("apiKey", getApiKey());
 
-        return map;
-    }
+		return map;
+	}
 
-    private String buildWebQuery(Map<String, String> parameters) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, String> entry : parameters.entrySet()) {
-            String key = URLEncoder.encode(entry.getKey(), "UTF-8");
-            String value = URLEncoder.encode(entry.getValue(), "UTF-8");
-            sb.append(key).append("=").append(value).append("&");
-        }
-        return sb.toString().substring(0, sb.length() - 1);
-    }
+	private String buildWebQuery(Map<String, String> parameters)
+			throws Exception {
+		StringBuilder sb = new StringBuilder();
+		for (Map.Entry<String, String> entry : parameters.entrySet()) {
+			String key = URLEncoder.encode(entry.getKey(), "UTF-8");
+			String value = URLEncoder.encode(entry.getValue(), "UTF-8");
+			sb.append(key).append("=").append(value).append("&");
+		}
+		return sb.toString().substring(0, sb.length() - 1);
+	}
 
-    private String callRestfulWebService(Map<String, String> parameters, String body) throws Exception {
-        String response = null;
-        final String queryString = buildWebQuery(parameters);
+	private String callRestfulWebService(Map<String, String> parameters,
+			String body) throws Exception {
+		String response = null;
+		final String queryString = buildWebQuery(parameters);
 
-        URL netUrl = new URL(url + "?" + queryString);
+		URL netUrl = new URL(url + "?" + queryString);
 
-        // Make post mode connection
-        URLConnection connection = null;
-        if (proxy == null) {
-            connection = netUrl.openConnection();
-        } else {
-            connection = netUrl.openConnection(proxy);
-        }
-        connection.setDoInput(true);
-        connection.setDoOutput(true);
-        connection.setAllowUserInteraction(false);
-        connection.setUseCaches(false);
+		// Make post mode connection
+		URLConnection connection = null;
+		if (proxy == null) {
+			connection = netUrl.openConnection();
+		} else {
+			connection = netUrl.openConnection(proxy);
+		}
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+		connection.setAllowUserInteraction(false);
+		connection.setUseCaches(false);
 
-        // Send query
-        PrintStream ps = new PrintStream(connection.getOutputStream());
-        ps.print(body);
-        ps.close();
+		// Send query
+		PrintStream ps = new PrintStream(connection.getOutputStream());
+		ps.print(body);
+		ps.close();
 
-        // Retrieve result
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-                sb.append("\n");
-            }
-            response = sb.toString();
-        } finally {
-            if (br != null) {
-                br.close();
-            }
-        }
+		// Retrieve result
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(
+					connection.getInputStream(), "UTF-8"));
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+				sb.append("\n");
+			}
+			response = sb.toString();
+		} finally {
+			if (br != null) {
+				br.close();
+			}
+		}
 
-        return response;
-    }
+		return response;
+	}
 }
